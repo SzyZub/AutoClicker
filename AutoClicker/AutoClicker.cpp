e22b9d6f25a8a719a9db5808406d8520690ea977
Szymon Żuber <tornaos21@gmail.com>
2024-05-29 12:39:23 +0200

    implemented getting pos

--- AutoClicker/AutoClicker.cpp
+++ AutoClicker/AutoClicker.cpp
@@ -1,29 +1,52 @@
 #include <wx/wx.h>
+#include <wx/spinctrl.h>
 #include <Windows.h>
 #include <chrono>
 #include <thread>
 
+#ifndef OCR_NORMAL
+#define OCR_NORMAL 32512
+#endif
+#ifndef OCR_HAND
+#define OCR_HAND 32649
+#endif
+
+
 class MyFrame : public wxFrame
 {
     HHOOK hook;
+    HHOOK MouseHook;
     int timer, type;
-    static int press;
+    int press;
     std::thread clicking;
     static MyFrame* instance;
+    wxBoxSizer* vertBox;
+    wxPanel* panel;
+    bool triggerNextClickAction;
 public:
     MyFrame() : wxFrame(nullptr, wxID_ANY, "AutoClicker", wxDefaultPosition, wxSize(800,400)) {
         Center();
         _mainFunc();
         instance = this;
         timer = 1000;
+        triggerNextClickAction = false;
         hook = SetWindowsHookExA(WH_KEYBOARD_LL, _Call, NULL, 0);
+        MouseHook = SetWindowsHookEx(WH_MOUSE_LL, _MouseProc, NULL, 0);
     };
     void _mainFunc() {
-        wxPanel* panel = new wxPanel(this, -1);
-        wxBoxSizer* vertBox = new wxBoxSizer(wxVERTICAL);
+        panel = new wxPanel(this, -1);
+        vertBox = new wxBoxSizer(wxVERTICAL);
         panel->SetSizer(vertBox);
+        _DrawInstructions();
+        _DrawSLider();
+        _DrawLoopType();
+        _DrawMouseButton();
+    }
+    void _DrawInstructions() {
         wxStaticText* info = new wxStaticText(panel, wxID_ANY, "Clicking F4 makes you toggle on/off the autoclicker");
         vertBox->Add(info, 1, wxALL | wxALIGN_CENTRE_HORIZONTAL, 0);
+    }
+    void _DrawSLider() {
         wxBoxSizer* timBox = new wxBoxSizer(wxVERTICAL);
         wxSlider* tim = new wxSlider(panel, 2, 1000, 5, 10000, wxDefaultPosition, wxDefaultSize, wxSL_LABELS);
         wxStaticText* timLabel = new wxStaticText(panel, wxID_ANY, "Miliseconds between clicks");
@@ -31,7 +54,33 @@ public:
         timBox->Add(tim, 5, wxEXPAND, 0);
         tim->Bind(wxEVT_SLIDER, &MyFrame::_OnSlider, this);
         vertBox->Add(timBox, 3, wxEXPAND, 0);
-        wxString arr[] = {"Left Mouse Button", "Middle Mouse Button", "Right Mouse Button"};
+    }
+    void _DrawLoopType() {
+        wxBoxSizer* TypeBox = new wxBoxSizer(wxHORIZONTAL);
+        wxBoxSizer* LeftTypeBox = new wxBoxSizer(wxVERTICAL);
+        wxBoxSizer* RightTypeBox = new wxBoxSizer(wxVERTICAL);
+        wxCheckBox* amountCheck = new wxCheckBox(panel, 4, "Click a certain amount of times");
+        wxSpinCtrl* amount = new wxSpinCtrl(panel, 5, "", wxDefaultPosition, wxDefaultSize, wxSP_ARROW_KEYS | wxSP_WRAP, 1, 10000);
+        wxCheckBox* posCheck = new wxCheckBox(panel, 6, "Click at this position");
+        wxSpinCtrl* x = new wxSpinCtrl(panel, 7, "x", wxDefaultPosition, wxDefaultSize,  wxSP_WRAP, 0, 4000);
+        wxSpinCtrl* y = new wxSpinCtrl(panel, 8, "y", wxDefaultPosition, wxDefaultSize,  wxSP_WRAP, 0, 2000);
+        wxButton* aim = new wxButton(panel, 9, "Indicate a position");
+        LeftTypeBox->Add(amountCheck, 1, wxEXPAND, 0);
+        LeftTypeBox->Add(amount, 3, wxEXPAND, 0);
+        RightTypeBox->Add(posCheck, 1, wxEXPAND, 0);
+        RightTypeBox->Add(x, 1, wxEXPAND, 0);
+        RightTypeBox->Add(y, 1, wxEXPAND, 0);
+        RightTypeBox->Add(aim, 1, wxEXPAND, 0);
+        TypeBox->Add(LeftTypeBox, 1, wxEXPAND, 0);
+        TypeBox->Add(RightTypeBox, 1, wxEXPAND, 0);
+        vertBox->Add(TypeBox, 3, wxEXPAND, 0);
+        aim->Bind(wxEVT_BUTTON, &MyFrame::_MousePos, this);
+    }
+    void _MousePos(wxCommandEvent &event) {
+        triggerNextClickAction = true;
+    }
+    void _DrawMouseButton() {
+        wxString arr[] = { "Left Mouse Button", "Middle Mouse Button", "Right Mouse Button" };
         wxRadioBox* typeClick = new wxRadioBox(panel, 3, "Which mouse button should be clicked", wxDefaultPosition, wxDefaultSize, 3, arr);
         typeClick->Bind(wxEVT_RADIOBOX, &MyFrame::_OnRadio, this);
         vertBox->Add(typeClick, 3, wxEXPAND, 0);
@@ -85,7 +134,7 @@ public:
         if (wParam == WM_KEYDOWN && nCode == HC_ACTION)
         {
             if (key->vkCode == VK_F4) {
-                if (press == 1) {
+                if (instance->press == 1) {
                     instance->_stopClicking();
                 }
                 else {
@@ -95,10 +144,19 @@ public:
         }
         return CallNextHookEx(NULL, nCode, wParam, lParam);
     }
+    static LRESULT CALLBACK _MouseProc(int nCode, WPARAM wParam, LPARAM lParam) {
+        if ((nCode >= 0 && (wParam == WM_LBUTTONDOWN || wParam == WM_RBUTTONDOWN) && instance->triggerNextClickAction)) {
+            POINT pt;
+            GetCursorPos(&pt);
+            ((wxSpinCtrl*)FindWindowById(7))->SetValue(pt.x);
+            ((wxSpinCtrl*)FindWindowById(8))->SetValue(pt.y);
+            instance->triggerNextClickAction = false;
+        }
+        return CallNextHookEx(instance->MouseHook, nCode, wParam, lParam);
+    }
 };
 
 MyFrame* MyFrame::instance = nullptr;
-int MyFrame::press = 0;
 
 class MyApp : public wxApp
 {
